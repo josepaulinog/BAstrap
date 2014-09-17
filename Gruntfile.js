e64e55ec2238c8dd57983bf443db33e5cfa45408
Davo <davidgalavotti@gmail.com>
2014-09-17 15:17:31 -0300

    Agrego bourbon y Bootstrap SASS via Bower, agrego grunt y tareas de compilación de SASS

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/base.css': 'css/base.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            },
+            options: {
+                livereload: true
+            }
+        },
+        cssmin: {
+            css:{
+                src: 'css/base.css',
+                dest: 'css/base.min.css'
+            }
+        }
+
+    });
+
+
+    //Register modules to user    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    //Register tasks
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('deploy', ['cssmin']);
+};
